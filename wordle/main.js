d89ca10045f0e40245aad27d0bdf271194d5186a
Santiago Trini <santiagotrini@gmail.com>
2023-06-28 17:30:46 -0300

    wordle arreglos

--- wordle/main.js
+++ wordle/main.js
@@ -1,3 +1,4 @@
+let input = document.forms[0].word;
 // lista de palabras que te pueden tocar
 let dictionary = [
   'roman',
@@ -6,11 +7,41 @@ let dictionary = [
   'padre',
   'madre',
   'barco',
-  'laser'
+  'laser',
+  'appaz',
+  'rudaz',
+  'rojas',
+  'lopez',
+  'canto',
+  'cosas',
+  'zorro',
+  'burro',
+  'dados',
+  'viejo',
+  'yuyos',
+  'papas',
+  'palta',
+  'siete',
+  'messi',
+  'huevo',
+  'hueso',
+  'nuevo',
+  'muero',
+  'minga',
+  'fulbo',
+  'punta',
+  'asado',
+  'chori',
+  'panes',
+  'juego',
+  'tipos',
+  'gatos',
+  'perro',
+  'piojo'
 ];
 // elegir una al azar
 let answer = dictionary[Math.floor(Math.random() * dictionary.length)];
-console.log(answer); // para hacer trampa
+console.log('La respuesta es: ' + answer); // para hacer trampa
 let tries = 0;
 
 function handleSubmit() {
@@ -18,14 +49,11 @@ function handleSubmit() {
   let word = event.target.word.value;
   if (word.length != 5) return;
   tries++;
-  if (tries <= 6 && word != answer) {
-    makeWord(word);
-    gameOver();
-  }
+  makeWord(word);
   if (word == answer) {
-    makeWord(word);
     win();
   }
+  if (tries == 6 && word != answer) gameOver();
   event.target.word.value = '';
 }
 
@@ -33,7 +61,7 @@ function makeWord(word) {
   let row = document.createElement('div');
   row.classList.add('row');
   for (let i = 0; i < word.length; i++) {
-    console.log(word[i]);
+    // console.log(word[i]);
     let l = document.createElement('h1');
     l.classList.add('letter');
     l.textContent = word[i];
@@ -48,10 +76,12 @@ function gameOver() {
   let gameOverMessage = document.createElement('h1');
   gameOverMessage.textContent = 'Perdiste!';
   document.body.prepend(gameOverMessage);
+  input.disabled = true;
 }
 
 function win() {
   let winMessage = document.createElement('h1');
   winMessage.textContent = 'Sos un genio!';
   document.body.prepend(winMessage);
+  input.disabled = true;
 }
