3eab23b4b2fe3f519955ed582b48adebb983d572
Santiago Trini <santiagotrini@gmail.com>
2024-08-12 16:57:29 -0300

    agrega el de codigo morse

--- morse/main.js
+++ morse/main.js
@@ -1,1 +1,33 @@
 // hacelo vos
+
+let input = document.querySelector('input');
+input.value = '';
+input.addEventListener('input', e => {
+  let phrase = e.target.value;
+  let result = document.querySelector('#result');
+  phrase = toMorse(phrase);
+  result.textContent = phrase;
+});
+
+
+
+let toMorse = phrase => {
+  // let morseTable = [
+  //   '.-', '-...', '-.-.', '-..', '.', '..-.', '--.','....'
+  // ];
+  let morseTable = {
+    a: '.-', b: '-...', c: '-.-.', d: '-..', 
+    e: '.', f: '..-.', g: '--.', h: '....',
+    i: '..', j: '.---', k: '-.-', l: '.-..',
+    m: '--', n: '-.', o: '---', p: '.--.',
+    q: '--.-', r: '.-.', s: '...', t: '-',
+    u: '..-', v: '...-', w: '.--', x: '-..-',
+    y: '-.--', z: '--..'
+  };
+  let result = '';
+  for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] != ' ')
+      result += morseTable[phrase[i].toLowerCase()] + ' '; 
+  }
+  return result;
+};
